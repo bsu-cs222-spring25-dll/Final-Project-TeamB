c68f69feace6988684fe1cdbdeac05bb56090de2
Oij13 <oij13@icloud.com>
2025-03-13 11:33:54 -0400

    Adds scroll wheel to scene

--- src/main/java/bsu/edu/cs/FuelComparisonApp.java
+++ src/main/java/bsu/edu/cs/FuelComparisonApp.java
@@ -4,6 +4,7 @@ import bsu.edu.cs.view.MainView;
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -13,16 +14,12 @@ public class FuelComparisonApp extends Application {
     public void start(Stage primaryStage){
         MainView mainView = new MainView();
 
-        Scene scene = new Scene(mainView);
-
         //scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/styles.css")).toExternalForm());
 
-        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setContent(mainView);
 
-        primaryStage.setX(screenBounds.getMinX());
-        primaryStage.setY(screenBounds.getMinY());
-        primaryStage.setWidth(screenBounds.getWidth());
-        primaryStage.setHeight(screenBounds.getHeight());
+        Scene scene = new Scene(scrollPane);
 
         primaryStage.setResizable(true);
 
