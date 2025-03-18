e716471fdf928b005d1160c00a2d884a02754bd0
Oij13 <oij13@icloud.com>
2025-03-18 12:15:56 -0400

    Fixes graph compiling

--- src/main/java/bsu/edu/cs/FuelComparisonApp.java
+++ src/main/java/bsu/edu/cs/FuelComparisonApp.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -16,23 +17,26 @@ public class FuelComparisonApp extends Application {
     public void start(Stage primaryStage){
         MainView mainView = new MainView();
 
+        BorderPane borderPane = new BorderPane();
+
 
 
         ScrollPane scrollPane = new ScrollPane();
         scrollPane.setContent(mainView);
 
-        Scene scene = new Scene(scrollPane, 600,800);
-        //scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("styles.css")).toExternalForm());
+        Scene scene = new Scene(scrollPane, 800,800);
 
         scene.getStylesheets().add("styles.css");
 
         primaryStage.setResizable(true);
 
 
+
         primaryStage.setTitle("Vehicle Fuel Economy Comparison");
 
         primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
     public static void main(String[] args){
